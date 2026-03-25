00721d281e48b836a8e1be505cc7c5536491a00d
AlexandraDigital <futuresuccess105@gmail.com>
2026-03-25 12:00:01 -0400

    Update search.js

--- functions/search.js
+++ functions/search.js
@@ -5,7 +5,7 @@ export async function onRequestGet({ url, env }) {
   try {
     // 🔴 1. Try YouTube first
     let ytRes = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${encodeURIComponent(q)}&key=${env.YT_API_KEY}&maxResults=1`
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&q=${encodeURIComponent(q)}&key=${env.YOUTUBE_API_KEY}&maxResults=1`
     );
     let ytData = await ytRes.json();
     let vid = ytData.items?.[0];
